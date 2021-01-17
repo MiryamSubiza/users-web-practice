7469f287b41036aabff7f10dd37f7b14605a2163
MiryamSubiza <miryam_247@hotmail.com>
2021-01-17 14:28:46 +0100

    Add persistent memory to sign up and log in
    Signed-off-by: MiryamSubiza <miryam_247@hotmail.com>

--- src/app/core/services/authentication.service.ts
+++ src/app/core/services/authentication.service.ts
@@ -1,9 +1,12 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 
+import { Credentials } from 'src/app/log-in/models/credentials';
 import { IUser } from 'src/app/users/models/User';
+import { environment } from 'src/environments/environment';
+import { logInUrl, signUpUrl } from '../routes';
 
 let users: IUser[] = JSON.parse(localStorage.getItem('users')) || [];
 
@@ -13,7 +16,6 @@ let users: IUser[] = JSON.parse(localStorage.getItem('users')) || [];
 export class AuthenticationService {
 
     private static LOCAL_STORAGE_TOKEN_KEY = 'token';
-    private mode = 'VOLATILE';
 
     private tokenSource = new BehaviorSubject<string>(null);
     public token$ = this.tokenSource.asObservable();
@@ -66,16 +68,39 @@ export class AuthenticationService {
         return !!this.localStorageToken;
     }
 
-    public logIn(email: string, password: string): void {
-        if (this.mode === 'VOLATILE') {
-            const user = users.find((user) => user.email === email && user.password === password);
+    public set errorMessage(errorMessage: string) {
+        this.errorMessageSource.next(errorMessage);
+    }
+
+    public logIn(credentials: Credentials): void {
+        if (environment.memory === 'persistent') {
+            this.http.post<boolean>(logInUrl, credentials)
+                .subscribe(() => {
+                    this.token = credentials.email;
+                    this.router.navigate(['/']);
+                    this.errorMessage = null;
+                }, (error) => {
+                    switch (error && error instanceof HttpErrorResponse && error.status) {
+                        case 401:
+                            this.errorMessage = 'Correo electrónico o contraseña incorrectos.';
+                            break;
+                        case 500:
+                            this.errorMessage = 'Error iniciando sesión, inténtelo más tarde.';
+                            break;
+                        default:
+                            break;
+                    }
+                    this.token = null;
+                });
+        } else if (environment.memory === 'volatile') {
+            const user = users.find((user) => user.email === credentials.email && user.password === credentials.password);
             if (user === undefined) {
                 this.token = null;
-                this.errorMessageSource.next('Correo electrónico o contraseña incorrectos.');
+                this.errorMessage = 'Correo electrónico o contraseña incorrectos.';
             } else {
                 this.token = user.email;
-                this.router.navigate(['/']);                
-                this.errorMessageSource.next(null);
+                this.router.navigate(['/']);
+                this.errorMessage = null;
             }
         }
     }
@@ -83,19 +108,40 @@ export class AuthenticationService {
     logOut() {
         this.token = null;
         this.router.navigate(['/login']);
+        this.errorMessage = null;
     }
 
     signUp(user: IUser): void {
-        if (this.mode === 'VOLATILE') {
-            if (users.find((u) => u.email === user.email) !== undefined) {
-                this.errorMessageSource.next('Ya existe un usuario con este correo electrónico.');
+        if (user) {
+            if (environment.memory === 'persistent') {
+                this.http.post<IUser>(signUpUrl, user)
+                    .subscribe((user) => {
+                        if (user) {
+                            this.errorMessage = null;
+                            this.logIn({ email: user.email, password: user.password } as Credentials);
+                        }
+                    }, (error) => {
+                        switch (error && error instanceof HttpErrorResponse && error.status) {
+                            case 409:
+                                this.errorMessage = 'Ya existe un usuario con este correo electrónico.';
+                                break;
+                            case 500:
+                                this.errorMessage = 'Error registrando usuario, inténtelo más tarde.';
+                                break;
+                            default:
+                                break;
+                        }
+                    });
             } else {
-                users.push(user);
-                localStorage.setItem('users', JSON.stringify(users));
-                this.errorMessageSource.next(null);
-                this.logIn(user.email, user.password);
+                if (users.find((u) => u.email === user.email) === undefined) {
+                    users.push(user);
+                    localStorage.setItem('users', JSON.stringify(users));
+                    this.errorMessageSource = null;
+                    this.logIn({ email: user.email, password: user.password } as Credentials);
+                } else {
+                    this.errorMessage = 'Ya existe un usuario con este correo electrónico.';
+                }
             }
-        } else {
         }
     }
 }
