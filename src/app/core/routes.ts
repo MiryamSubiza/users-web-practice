849d8a71677bc7a3d27f1101ab324a2857259ab7
MiryamSubiza <miryam_247@hotmail.com>
2021-01-18 14:18:56 +0100

    Update API routes
    Signed-off-by: MiryamSubiza <miryam_247@hotmail.com>

--- src/app/core/routes.ts
+++ src/app/core/routes.ts
@@ -1,6 +1,6 @@
 import { environment } from "src/environments/environment";
 
 export const apiUrl = environment.apiUrl;
-export const logInUrl = `${apiUrl}/users/login`;
-export const signUpUrl = `${apiUrl}/users/postUser`;
-export const usersUrl = `${apiUrl}/users/fromdb`;
+export const usersUrl = `${apiUrl}/users`;
+export const logInUrl = `${usersUrl}/login`;
+export const signUpUrl = `${usersUrl}/signup`;
